aabd1fe59ed020c3d3a5976cb2015ef23681edad
Rosalind Whittam <Rosalind.Whittam@bbc.co.uk>
2021-12-13 16:42:18 +0000

    Added detection for flicking events Added threshold for rotation level so that gyro only registers when reaching a certain angle of rotation with the flick

--- app/src/main/java/com/example/hack_week_100_treasures/GuessActivity.kt
+++ app/src/main/java/com/example/hack_week_100_treasures/GuessActivity.kt
@@ -7,15 +7,22 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hack_week_100_treasures.databinding.ActivityGuessBinding
+import android.os.Looper
+
+
+
 
 class GuessActivity : AppCompatActivity(), SensorEventListener {
 
     var rotationSensor: Sensor? = null
     var gyroSensor: Sensor? = null
+    var sensorReactionEnabled = true
+    var rotationThresholdReached = false
 
     lateinit var sensorManager: SensorManager
     lateinit var binding: ActivityGuessBinding
@@ -64,25 +71,53 @@ class GuessActivity : AppCompatActivity(), SensorEventListener {
     override fun onSensorChanged(event: SensorEvent?) {
         if (event != null && event.sensor.type == Sensor.TYPE_ROTATION_VECTOR) {
             val xRotation = event.values[0]
-            Log.e("Value", "x: $xRotation")
+            //Log.e("Value", "x: $xRotation")
             //binding.textView2.text = xRotation.toString()
 
             when{
-                xRotation <= 0.3 -> binding.textView2.text = "Up"
-                xRotation >= 0.6 -> binding.textView2.text = "Down"
-                else -> binding.textView2.text = "Flat"
+                xRotation >= -0.2 -> {
+                    rotationThresholdReached = true
+                }
+                xRotation <= -0.8 -> {
+                    rotationThresholdReached = true
+                }
+                else -> {
+                    rotationThresholdReached = false
+                }
             }
         }
 
-//        if(event != null && event.sensor.type == Sensor.TYPE_GYROSCOPE){
-//            val xAxis = event.values[1]
-//            //binding.textView2.text = xAxis.toString()
-//            when{
-//                xAxis <= -0.5 -> binding.textView2.text = "Up"
-//                xAxis >= 0.5 -> binding.textView2.text = "Down"
-//                else -> binding.textView2.text = "Flat"
-//            }
-//        }
+        if(event != null && event.sensor.type == Sensor.TYPE_GYROSCOPE){
+            val xAxis = event.values[1]
+            //binding.textView2.text = xAxis.toString()
+            when{
+                xAxis <= -1 -> {
+                    if(sensorReactionEnabled) {
+                        Log.d("Gyro", "Rotation Threshold Reached: $rotationThresholdReached")
+                        sensorReactionEnabled = false
+                        val handler = Handler(Looper.getMainLooper())
+                        handler.postDelayed({ sensorReactionEnabled = true }, 1000)
+
+                        Log.d("Gyro", "Flicked Forward")
+                        binding.textView2.text = "Up"
+                    }
+                }
+                xAxis >= 1 -> {
+                    if(sensorReactionEnabled) {
+                        Log.d("Gyro", "Rotation Threshold Reached: $rotationThresholdReached")
+                        sensorReactionEnabled = false
+                        val handler = Handler(Looper.getMainLooper())
+                        handler.postDelayed({ sensorReactionEnabled = true }, 1000)
+
+                        Log.d("Gyro", "Flicked Back")
+                        binding.textView2.text = "Down"
+                    }
+                }
+                else -> {
+                    binding.textView2.text = "Flat"
+                }
+            }
+        }
 
     }
 
